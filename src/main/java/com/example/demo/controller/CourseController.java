11a6c494aedd9e59a039b913d175bec2591436fc
bala-amypo <bala@amypo.in>
2025-12-26 05:36:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -2,32 +2,24 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.model.Course;
 import com.example.demo.service.CourseService;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
-@Tag(name = "Courses")
 public class CourseController {
 
     @Autowired
     private CourseService courseService;
 
-    @Operation(summary = "Create course")
     @PostMapping
     public Course createCourse(@RequestBody Course course) {
         return courseService.saveCourse(course);
     }
 
     @PutMapping("/{courseId}")
-    public Course updateCourse(@PathVariable Long courseId,
-                               @RequestBody Course course) {
+    public Course updateCourse(@PathVariable Long courseId, @RequestBody Course course) {
         return courseService.updateCourse(courseId, course);
     }
 
